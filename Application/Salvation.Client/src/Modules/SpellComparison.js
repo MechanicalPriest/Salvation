8418401221e93f32ff2a9d1341b4c033a8d29261
Michael Wenham <niphyr@gmail.com>
2020-09-18 01:39:05 +1000

    Removed unneccessary imports and methods that are no longer used

--- Application/Salvation.Client/src/Modules/SpellComparison.js
+++ Application/Salvation.Client/src/Modules/SpellComparison.js
@@ -1,11 +1,6 @@
 import React from "react";
 
 // Stuff from Material-UI
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -19,15 +14,6 @@ import Accordion from '@material-ui/core/Accordion'
 import AccordionSummary from '@material-ui/core/AccordionSummary'
 import AccordionDetails from '@material-ui/core/AccordionDetails'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-
-
-const useRowStyles = makeStyles({
-  root: {
-    '& > *': {
-      borderBottom: 'unset',
-    },
-  },
-});
 
 function round(value, precision = 2) {
   return value.toFixed(precision);
@@ -60,8 +46,6 @@ function SubRow(props) {
 
 function Row(props) {
   const { row } = props;
-  const [open, setOpen] = React.useState(false);
-  const classes = useRowStyles();
 
   return (
     <React.Fragment>

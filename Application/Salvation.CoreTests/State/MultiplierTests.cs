9fd666d63e9967bb42c9400d8f5ecf4bbd11525d
itshawk <itshawk@gnode.org>
2020-10-23 14:31:54 -1000

    Added some test cases
    -- Some more test cases including DR options, nums are from the func itself

--- Application/Salvation.CoreTests/State/MultiplierTests.cs
+++ Application/Salvation.CoreTests/State/MultiplierTests.cs
@@ -37,49 +37,52 @@ namespace Salvation.CoreTests.State
             _gameStateService = new GameStateService();
         }
         [TestCaseSource(typeof(MultiplierTestData), nameof(MultiplierTestData.CritTestData))]
-        public double CritMultiplierTest()
+        public double CritMultiplierTest(int crit_val)
         {            
             // Arrange
 
 
             // Act
 
-
+            _state.Profile.CritRating = crit_val;
             // Assert
             return _gameStateService.GetCriticalStrikeMultiplier(_state);
         }
         [TestCaseSource(typeof(MultiplierTestData), nameof(MultiplierTestData.HasteTestData))]
-        public double HasteMultiplierTest()
+        public double HasteMultiplierTest(int haste_val)
         {
             // Arrange
 
 
             // Act
 
+            _state.Profile.HasteRating = haste_val;
 
             // Assert
             return _gameStateService.GetHasteMultiplier(_state);
         }
         [TestCaseSource(typeof(MultiplierTestData), nameof(MultiplierTestData.MasteryTestData))]
-        public double MasteryMultiplierTest()
+        public double MasteryMultiplierTest(int mastery_val)
         {
             // Arrange
 
 
             // Act
 
+            _state.Profile.MasteryRating = mastery_val;
 
             // Assert
             return _gameStateService.GetMasteryMultiplier(_state);
         }
         [TestCaseSource(typeof(MultiplierTestData), nameof(MultiplierTestData.VersTestData))]
-        public double VersMultiplierTest()
+        public double VersMultiplierTest(int vers_val)
         {
             // Arrange
 
 
             // Act
 
+            _state.Profile.VersatilityRating = vers_val;
 
             // Assert
             return _gameStateService.GetVersatilityMultiplier(_state);
@@ -93,28 +96,36 @@ namespace Salvation.CoreTests.State
         {
             get
             {
-                yield return new TestCaseData().Returns(1.1265714285714286d);
+                yield return new TestCaseData(100).Returns(1.0785714285714285d);
+                yield return new TestCaseData(1500).Returns(1.4628857142857221d); 
+                yield return new TestCaseData(300).Returns(1.1357142857142857d);
             }
         }
         public static IEnumerable HasteTestData
         {
             get
             {
-                yield return new TestCaseData().Returns(1.0733333333333333d);
+                yield return new TestCaseData(100).Returns(1.0303030303030303d);
+                yield return new TestCaseData(1500).Returns(1.433666666666672d);
+                yield return new TestCaseData(300).Returns(1.0909090909090908d);
             }
         }
         public static IEnumerable MasteryTestData
         {
             get
             {
-                yield return new TestCaseData().Returns(1.1864285714285716d);
+                yield return new TestCaseData(100).Returns(1.1357142857142859d);
+                yield return new TestCaseData(1500).Returns(1.5950357142857121d);
+                yield return new TestCaseData(300).Returns(1.2071428571428573d);
             }
         }
         public static IEnumerable VersTestData
         {
             get
             {
-                yield return new TestCaseData().Returns(1.0347500000000001d);
+                yield return new TestCaseData(100).Returns(1.0249999999999999d);
+                yield return new TestCaseData(1500).Returns(1.3675250000000068d);
+                yield return new TestCaseData(300).Returns(1.075d);
             }
         }
     }

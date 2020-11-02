3c2960364b521eec3b64795a63cdb5666e4f88ca
Michael Wenham <niphyr@gmail.com>
2020-11-02 21:52:06 +1100

    Added SpellEffectService base tests

---
+++ Application/Salvation.CoreTests/Common/SpellEffectServiceBaseTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Salvation.Core.Constants.Data;
+using Salvation.Core.Interfaces.State;
+using Salvation.Core.Modelling.Common;
+using Salvation.Core.State;
+
+namespace Salvation.CoreTests.Common
+{
+    [TestFixture]
+    public class SpellEffectServiceBaseTests : BaseTest
+    {
+        private GameState _gameState;
+
+        [OneTimeSetUp]
+        public void InitOnce()
+        {
+            _gameState = GetGameState();
+        }
+
+        [Test]
+        public void Int_Returns_Zero()
+        {
+            // Arrange
+            IGameStateService gameStateService = new GameStateService();
+            var spellService = new SpellEffectService(gameStateService);
+
+            // Act
+            var result = spellService.GetAverageIntellect(_gameState, null);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+    }
+}

345c55c214a3fde074df58060c3b769a2c1498cc
Niphyr <Niphyr@users.noreply.github.com>
2020-11-17 03:34:56 +1100

    Added display of stat weights

--- Application/Salvation.Client/src/Modules/Results/index.js
+++ Application/Salvation.Client/src/Modules/Results/index.js
@@ -8,7 +8,17 @@ const Results = () => {
     <div>
       <p>Results go here</p>
       {baseResults.modelResults !== undefined &&
+      <div>
         <p>HPS: {baseResults.modelResults.totalActualHPS}</p>
+      
+        <p>Stat Weights - {baseResults.statWeightsEffective.name}</p>
+        {baseResults.statWeightsEffective.results.map((weight) => {
+          return (
+            <p>{weight.stat}: {parseFloat(weight.weight).toFixed(2)}</p>
+          );
+        })}
+        <p></p>
+      </div>
       }
     </div>
   );
